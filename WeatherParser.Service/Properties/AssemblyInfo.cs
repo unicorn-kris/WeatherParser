d14b835f5096fd693fb7584bda3c95b6eeacdbbb
kchernova <kchernova@syntellect.ru>
2022-09-21 13:44:42 +0400

    Replace folder name (servicee -> service) (not working!!)

--- WeatherParser.Service/Properties/AssemblyInfo.cs
+++ WeatherParser.Service/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные со сборкой.
-[assembly: AssemblyTitle("WeatherParser.Servicee")]
+[assembly: AssemblyTitle("WeatherParser.Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WeatherParser.Servicee")]
+[assembly: AssemblyProduct("WeatherParser.Service")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
